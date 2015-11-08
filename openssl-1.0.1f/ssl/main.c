aed43fe156eac9dad88541205612c449e4a52607
RENESYS <hrs350x@naver.com>
2015-11-08 13:19:13 +0900

    openssl 1.0.1f

---
+++ openssl-1.0.1f/ssl/main.c
@@ -0,0 +1,16 @@
+/*
+
+	main.c
+
+	This is test file for searching weakness part of OpenSSL
+	The weakness part is including at 'd1_both.c'
+
+*/
+
+
+#include "d1_both.c"
+
+int main(){
+
+	return 0;
+}
